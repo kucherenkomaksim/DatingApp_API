6d0dd46fe2e29cdf0f8ceece2d177b2fe7e39c48
Maksym Kucherenko <92512345+kucherenkomaksim@users.noreply.github.com>
2023-06-13 17:17:53 +0300

    Added packages and ssl to Angular

--- API/Program.cs
+++ API/Program.cs
@@ -13,11 +13,15 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors(opt => opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
